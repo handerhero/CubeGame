cca23ee336e4100390c7e5a2d6dbfb8cbcd421f0
handerhero <65731332+handerhero@users.noreply.github.com>
2026-01-14 14:56:05 +0300

    Delete Headers directory

--- Headers/Configuration/Constants.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-constexpr int BLOCK_AIR = 0;
-constexpr int BLOCK_GRASS = 5;
-constexpr int BLOCK_DIRT = 1;
-constexpr int BLOCK_STONE = 3;
-constexpr int CHUNK_SIZE_X = 32, CHUNK_SIZE_Y = 32, CHUNK_SIZE_Z = 32;
-constexpr int CHUNK_SIZE = 32;
-
